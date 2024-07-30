b295effa7635cf40c2b38959b68a62c5afd31ddb
Alexei Lubinets <lubinets01@mail.ru>
2024-07-30 23:08:26 +0500

    planechange

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20_000;
+        int bonusmile = 20;
+        System.out.println(ticketPrice / bonusmile);
+    }
+}
